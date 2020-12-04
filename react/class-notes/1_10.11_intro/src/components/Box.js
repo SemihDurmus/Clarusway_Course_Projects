8527204acb92c320e92f5c0065e6204947a765d2
SemihDurmus <semihdurmus@yahoo.com>
2020-12-04 01:30:30 +0100

    Delete unnecesssary files

--- react/class-notes/1_10.11_intro/src/components/Box.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-class Box extends React.Component {
-  render() {
-    return <div className="box-wrapper">{this.props.name || "BOX"}</div>;
-  }
-}
-export default Box;
